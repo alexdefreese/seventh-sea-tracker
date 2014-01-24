a846a77ef67d94ac2fff4c974ceda23caccde154
William DeFreese <alexdefreese@gmail.com>
2014-01-23 20:12:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeventhSeaTracker::Application.config.secret_key_base = '8e32a4ccfe3be1a85f493327c178c23c88c2e5a31f656f362e3709f4841881e0fce955e64f43ab09cf3b072de6cb8fe7c24740672cb302f7a05f85b3bce3557f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeventhSeaTracker::Application.config.session_store :cookie_store, key: '_seventh-sea-tracker_session'
