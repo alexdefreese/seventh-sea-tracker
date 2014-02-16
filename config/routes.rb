0a5bc83d1e079c69fc84bc83a8a8e6bcedadee98
Alex DeFreese <alexdefreese@gmail.com>
2014-02-16 13:42:41 -0500

    Initialized CharactersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SeventhSeaTracker::Application.routes.draw do
+  resources :characters
   devise_for :users
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
